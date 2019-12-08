446c979814c7564a6114393d1a2d4ff0e277a2f1
Vanessa <vanessa.wiedeman.cs@gmail.com>
2019-12-07 19:02:58 -0600

    setup/user input

--- test.js
+++ test.js
@@ -1,7 +1,26 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+
 var inputString = process.argv;
 
 var option = inputString[2];
 
 var search = process.argv.slice(3).join("+");
 
-console.log(search);
+if(option == "concert-this"){
+    console.log("1");
+}
+else if(option == "movie-this"){
+    console.log("2");
+}
+else if(option == "spotify-this-song"){
+    console.log("3");
+}
+else if(option == "do-what-it-says"){
+    console.log("4");
+}
+else{
+    console.log("Sorry that is out of my commands.");
+}
